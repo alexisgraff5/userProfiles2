83c06b82552b5ce626bab558830dbed117a534cd
alexisgraff5 <lex.graff@gmail.com>
2016-10-26 12:33:07 -0600

    Created pages to go through data

--- controller.js
+++ controller.js
@@ -1,8 +1,29 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+
+  $scope.currentPage = 1;
+  $scope.maxPages = 2;
+
   $scope.getUsers = function() {
-    mainService.getUsers().then(function(response) {
+    var promise = mainService.getUsers($scope.currentPage);
+      promise.then(function(response) {
       $scope.users = response.data.data;
+      $scope.maxPages = response.data.total_pages;
     });
   }
   $scope.getUsers();
+
+  $scope.prev = function(){
+    if($scope.currentPage > 1){
+      $scope.currentPage--;
+      $scope.getUsers();
+    }
+  }
+
+  $scope.next = function(){
+    if($scope.currentPage < $scope.maxPages){
+      $scope.currentPage++;
+      $scope.getUsers();
+    }
+  }
+
 });
